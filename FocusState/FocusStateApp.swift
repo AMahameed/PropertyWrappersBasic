5c1c972ac160c6de301518ba092eb5b24000a30d
Abdallah Mahameed <abdallah.mahameed@opensooq.com>
2024-09-18 12:56:22 +0300

    Initial Commit

---
+++ FocusState/FocusStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusStateApp.swift
+//  FocusState
+//
+//  Created by Abdallah Mahameed on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FocusStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
